5e614e71659851259351ef3479f27ddc56aa2844
murch <ryan.c.murchie@gmail.com>
2017-01-06 13:07:04 -0500

    Function skeleton

--- pedigreeCompile.R
+++ pedigreeCompile.R
@@ -4,3 +4,12 @@
 #Used to amalgamate pedigree information into standard format
 #
 #
+#arguments:
+#   pedData - data.frame containing pedigree information (ID, proband ID, gender, affection status)
+#   
+
+pedigreeCompile <-function(pedData)
+{
+ 
+  
+}
