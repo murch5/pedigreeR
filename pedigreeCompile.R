10d9e58ce82b50fbb2c0b620404f940d3ceac05b
murch <ryan.c.murchie@gmail.com>
2017-01-13 12:02:50 -0500

    Assign unique IDs to var Comments

--- pedigreeCompile.R
+++ pedigreeCompile.R
@@ -1,15 +1,20 @@
 #
-#PEDIGREE COMPILE FUNCTION
+#COMPILE PEDIGREE FUNCTION
 #
 #Used to amalgamate pedigree information into standard format
 #
+# -parses phenotyping file with multiple individual entries for each trio (will only take unique entries)
+# -returns IDs with conflicts (gender,relationship etc)
 #
 #arguments:
 #   pedData - data.frame containing pedigree information (ID, proband ID, gender, affection status)
 #   
 
-pedigreeCompile <-function(pedData)
+compilePedigree <-function(pedData)
 {
  
+  uniqueID <- unique(pedData[,"ID"])  #isolated only unique ID values 
+  
+  
   
 }
