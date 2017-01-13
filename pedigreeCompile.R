d9e6630e93a0363d472b32c18289e3fe8b7554ff
murch <ryan.c.murchie@gmail.com>
2017-01-13 14:48:22 -0500

    add unique ID loop extract subset of entries with same ID and famID

--- pedigreeCompile.R
+++ pedigreeCompile.R
@@ -9,12 +9,40 @@
 #arguments:
 #   pedData - data.frame containing pedigree information (ID, proband ID, gender, affection status)
 #   
+#Family ID, Individual ID, Paternal ID, Maternal ID, Sex (1=male; 2=female; other=unknown), Phenotype
 
 compilePedigree <-function(pedData)
 {
- 
-  uniqueID <- unique(pedData[,"ID"])  #isolated only unique ID values 
+  conflictList <- data.frame()
+  
+  print("Compile Pedigree Start")
+  
+  uniqueID <- unique(pedData[,"ID"])  #isolated only unique ID values
+  
+  pedigree <- lapply(uniqueID, function(y){
+    
+      subset <- unique(unique(pedData[which(pedData[,"ID"] == y || pedData[,"FamilyID"] == y),])) #isolate subset of file entries sharing same ID
+      
+      famID <- unique(subset[,"FamilyID"])
+      paternalID <- unique(subset[,"paternalID"])
+      maternalID <- unique(subset[,"maternalID"])
+      sexID <- unique(subset[,"sexID"])
+      affectionID <- unique(subset[,"affectionID"])
+      
+      if(nrow(famID) > 1)
+      {
+        #Conflicting Family IDs
+        
+      }
+      
+      paternalID <- unique(subset)
+      
+      
+      return(entry)
+    
+    
+  })
   
   
-  
+  return(pedigree)
 }
