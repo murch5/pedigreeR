4b921f29d40d3d70136f3f8aae166835fd311b75
murch <ryan.c.murchie@gmail.com>
2017-01-14 18:43:17 -0500

    redid compile pedigree function to iterate through individual IDs in for loop assigns values to vector that is rbind to pedigree df if ID does not exist, return zeros

--- pedigreeCompile.R
+++ pedigreeCompile.R
@@ -17,30 +17,51 @@ compilePedigree <-function(pedData)
   print("Compile Pedigree Start")
   
   uniqueID <- unique(pedData[,"ID"])  #isolated only unique ID values
+
+  pedigree <- data.frame(ID = character(),
+                         famID = numeric(),
+                         paternalID = numeric(),
+                         maternalID = numeric(),
+                         sexID = numeric(),
+                         affectionID = numeric())
   
-  
-  pedigree <-lapply(uniqueID, function(y){
-    
-      subset <- unique(pedData[which(pedData[,"ID"] == y),]) #isolate subset of file entries sharing same ID
-      famSubset <- unique(pedData[which(pedData[,"familyID"] == y),]) #isolate family members
+  for(i in 1:length(uniqueID))
+  {
+
+      subset <- unique(pedData[which(pedData[,"ID"] == uniqueID[i]),]) #isolate subset of file entries sharing same ID
+      famSubset <- unique(pedData[which(pedData[,"familyID"] == uniqueID[i]),])
       
+      ID <- uniqueID[i] #the ID of the individual entry is iterator value
+      
+      if(length(subset[,"familyID"])>1)
+      {
+        print("Error: Multiple Family ID Detected for" + uniqueID[i])
+      }
+      
+      famID <- subset[,"familyID"] #initially set famID to index ID *** MAY BE OVERWRITTEN LATER
+      if(length(famID)==0 || is.na(famID)) {famID = 0;}
+  
       paternalID <- famSubset[which(famSubset[,"familyStatus"]=="Father"),"ID"]
       maternalID <- famSubset[which(famSubset[,"familyStatus"]=="Mother"),"ID"]
+      
+      if(length(paternalID)==0){paternalID = 0; }
+      if(length(maternalID)==0){maternalID = 0;}
       
       sexID <- unique(subset[,"genderID"])
       affectionID <- unique(subset[,"affectionID"])
 
-      entry <- c(y,affectionID)
+      entry <- c(famID,ID,paternalID,maternalID,sexID,affectionID)
       
-      return(entry)
-    
-  })
-  
- 
+      
+      pedigree<-rbind(pedigree,entry)
+      
+  }
+  colnames(pedigree) <- c("famID","ID","paternalID","maternalID","sexID","affectionID")
   return(pedigree)
 }
 
 test <- read.csv("test.csv")
-pruned <- test[,c(4,5,6,7,10)]
+pruned <- test[,c(3,4,5,6,9)]
 colnames(pruned) <- c("ID","familyID","familyStatus","genderID","affectionID")
 pedigree <- compilePedigree(pruned)
+
