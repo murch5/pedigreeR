5ac74242dca8c906d896cdd598a5a97a31d6b967
murch <ryan.c.murchie@gmail.com>
2017-01-14 20:53:07 -0500

    func skeleton comment header

---
+++ pedigreeClassify.R
@@ -0,0 +1,20 @@
+#
+#CLASSIFY PEDIGREE
+#
+#Classifies and subtypes pedigree structures in formatted pedigree file
+#
+# -passed formatted pedigree spreadsheet
+# -returns a data.frame with one row per family with pedigree structure details
+#
+#arguments:
+#   pedigree - formatted pedigree file from pedigreeCompile
+#   
+#returns:
+#
+#   familyPed - family specific pedigree data (family type, members present, structure, total number)
+
+classifyPedigree <- function(pedigree)
+{
+  
+  
+}
