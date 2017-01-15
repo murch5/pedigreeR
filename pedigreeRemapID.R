5fb9241a13a2c47219679fde95f8d06f1496310c
murch <ryan.c.murchie@gmail.com>
2017-01-14 20:44:30 -0500

    Code layout Remap ID func skeleton Comments

---
+++ pedigreeRemapID.R
@@ -0,0 +1,21 @@
+#
+#REMAP ID FUNCTION
+#
+#Used to remap IDs in pedigree/phenotyping file
+#
+# -passed ID value and map as arugments and returns remapped ID value (or 0 is no match present)
+# 
+#
+#arguments:
+#   oldID - ID number to be remapped
+#   map - data.frame containing two columns matching old and new IDs
+#
+#returns:
+#
+#   newID - remapped ID
+
+remapID <- function(oldID,map)
+{
+  
+  
+}
